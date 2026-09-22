9825c36de80ad170b567274aa69464c2c904dff5
Chaitanya <csbd06@gmail.com>
2026-09-23 01:11:21 +0530

    nodemailer

--- src/config/nodemail.js
+++ src/config/nodemail.js
@@ -1,27 +1,42 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASSWORD,
-    },
-})
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
 
 export const sendMail = async ({ to, subject, html }) => {
-    try {
-        console.log(to,subject)
-        const info = await transporter.sendMail({
-            from: `"Brush & Colours" <${process.env.MAIL_USER}>`,
-            to,
-            subject,
-            html,
-        })
+  try {
+    console.log("Sending email:", {
+      to,
+      subject,
+      mailUser: process.env.MAIL_USER,
+      passwordExists: !!process.env.MAIL_PASSWORD,
+    });
 
-        console.log('Email sent:', info.messageId)
-        return info
-    } catch (error) {
-        console.error('Email sending failed:', error)
-        throw error
-    }
-}
+    const info = await transporter.sendMail({
+      from: `"Brush & Colours" <${process.env.MAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent:", info.messageId);
+
+    return info;
+  } catch (error) {
+    console.error("Email sending failed:", {
+      message: error.message,
+      code: error.code,
+      command: error.command,
+      response: error.response,
+    });
+
+    throw error;
+  }
+};
